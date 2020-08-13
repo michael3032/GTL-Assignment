25fdb376777799725a946705da1bf12f640ae9a5
michael3032 <68828379+michael3032@users.noreply.github.com>
2020-08-13 10:07:14 +0000

    Add files via upload

---
+++ HW3_7SR3ESP54I/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Juli from './Mike.js';
+
+import './css/components.css';
+import './css/global.css';
+import './css/layout.css';
+import './css/sample-wallpaper.css';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Juli/>
+      
+    </div>
+  );
+}
+
+export default App;
